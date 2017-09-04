1321786a20e168c24cd531510efd855419e2c0b8
Jason Collins <the.oem@gmail.com>
2017-09-04 14:20:10 -0600

    Refactor the file structure. Add ability for usernames.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const path = require('path');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
+
 
 io.on('connection', function(socket){
   socket.broadcast.emit('broadcast', 'New user connected.');
