905c43b231b26587a5f4b088c772777b78edaaf0
Jason Collins <the.oem@gmail.com>
2017-09-04 17:19:12 -0600

    Requirement: Don’t send the same message to the user that sent it himself. Instead, append the message directly as soon as he presses enter.

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const connectedUsers = [];
+
 const path = require('path');
 
 app.use(express.static('public'));
@@ -11,17 +13,17 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
 
-
 io.on('connection', function(socket){
+  // console.log(`User socket ${socket.id} connected.`);
   socket.broadcast.emit('broadcast', 'New user connected.');
 
   socket.on('disconnect', function(){
+    // console.log(`User socket ${socket.id} disconnected.`);
     socket.broadcast.emit('broadcast', 'A user disconnected.');
   });
 
   socket.on('chat message', function(msg){
-    // console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    socket.broadcast.emit('broadcast', msg);
   });
 });
 
